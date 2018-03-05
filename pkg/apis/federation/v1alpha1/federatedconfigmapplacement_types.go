fd3c1f06a20175f41960055db1dc695e3b7e8b45
Maru Newby <marun@redhat.com>
2018-03-05 07:49:39 -0800

    Add ClusterNames to FederatedConfigMapPlacement

--- pkg/apis/federation/v1alpha1/federatedconfigmapplacement_types.go
+++ pkg/apis/federation/v1alpha1/federatedconfigmapplacement_types.go
@@ -1,4 +1,3 @@
-
 /*
 Copyright 2018 The Kubernetes Authors.
 
@@ -14,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 
 package v1alpha1
 
@@ -46,6 +44,8 @@ type FederatedConfigMapPlacement struct {
 
 // FederatedConfigMapPlacementSpec defines the desired state of FederatedConfigMapPlacement
 type FederatedConfigMapPlacementSpec struct {
+	// Names of the clusters that a federated resource should exist in.
+	ClusterNames []string `json:"clusternames,omitempty"`
 }
 
 // FederatedConfigMapPlacementStatus defines the observed state of FederatedConfigMapPlacement
