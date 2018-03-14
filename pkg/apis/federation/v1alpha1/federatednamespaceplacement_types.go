b2eb57ba70e8080c9c7cd63c5233a75fe712f2ac
Ivan Font <ifont@redhat.com>
2018-03-13 22:21:54 -0700

    Add ClusterNames to FederatedNamespacePlacement

--- pkg/apis/federation/v1alpha1/federatednamespaceplacement_types.go
+++ pkg/apis/federation/v1alpha1/federatednamespaceplacement_types.go
@@ -1,4 +1,3 @@
-
 /*
 Copyright 2018 The Kubernetes Authors.
 
@@ -14,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 
 package v1alpha1
 
@@ -47,6 +45,8 @@ type FederatedNamespacePlacement struct {
 
 // FederatedNamespacePlacementSpec defines the desired state of FederatedNamespacePlacement
 type FederatedNamespacePlacementSpec struct {
+	// Names of the clusters that a federated resource should exist in.
+	ClusterNames []string `json:"clusternames,omitempty"`
 }
 
 // FederatedNamespacePlacementStatus defines the observed state of FederatedNamespacePlacement
