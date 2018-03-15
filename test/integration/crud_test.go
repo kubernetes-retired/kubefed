b350119f2d1f34b857f2133b793dddf794625ba4
Ivan Font <ifont@redhat.com>
2018-03-15 13:34:50 -0700

    Disable integration test for FederatedNamespace

--- test/integration/crud_test.go
+++ test/integration/crud_test.go
@@ -37,6 +37,12 @@ func TestCrud(t *testing.T) {
 
 	fedTypeConfigs := federatedtypes.FederatedTypeConfigs()
 	for kind, fedTypeConfig := range fedTypeConfigs {
+		// TODO (font): integration tests for FederatedNamespace does not work
+		// until namespace controller is added.
+		if kind == federatedtypes.FederatedNamespaceKind {
+			continue
+		}
+
 		t.Run(kind, func(t *testing.T) {
 			tl := framework.NewIntegrationLogger(t)
 			fixture, crudTester, template, placement, override := initCrudTest(tl, fedFixture, fedTypeConfig.AdapterFactory, kind)
