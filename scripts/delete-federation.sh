9cb902ef88b3dd2e5b91206f3f75b30958629120
Maru Newby <marun@redhat.com>
2018-04-21 18:41:28 -0700

    Add scripts for deploying and deleting test federations.

---
+++ scripts/delete-federation.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 The Federation v2 Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script removes the cluster registry and federation from the
+# current kubectl context.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source "$(dirname "${BASH_SOURCE}")/util.sh"
+
+KCD="kubectl --ignore-not-found=true delete"
+
+# Remove the federation service account for the current context.
+CONTEXT="$(kubectl config current-context)"
+SA_NAME="${CONTEXT}-${CONTEXT}"
+${KCD} -n federation sa "${SA_NAME}"
+${KCD} -n federation clusterrole "federation-controller-manager:${SA_NAME}"
+${KCD} -n federation clusterrolebinding "federation-controller-manager:${SA_NAME}"
+
+# Remove federation
+${KCD} apiservice v1alpha1.federation.k8s.io
+${KCD} namespace federation
+
+# Remove cluster registry
+${KCD} clusterrolebinding federation-admin
+${KCD} apiservice v1alpha1.clusterregistry.k8s.io
+${KCD} namespace clusterregistry
+${KCD} clusterrole clusterregistry.k8s.io:apiserver
+${KCD} clusterrolebinding clusterregistry.k8s.io:apiserver
+${KCD} clusterrolebinding clusterregistry.k8s.io:apiserver-auth-delegator
+${KCD} -n kube-system rolebinding clusterregistry.k8s.io:extension-apiserver-authentication-reader
+
+# Wait for the namespaces to be removed
+function ns-deleted() {
+  kubectl get ns "${1}" &> /dev/null
+  [[ "$?" = "1" ]]
+}
+util::wait-for-condition "removal of namespace 'federation'" "ns-deleted federation" 120
+util::wait-for-condition "removal of namespace 'clusterregistry'" "ns-deleted clusterregistry" 120
---
+++ scripts/util.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 The Federation v2 Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This library holds bash utility functions.
+
+# util::wait-for-condition blocks until the provided condition becomes true
+#
+# Globals:
+#  None
+# Arguments:
+#  - 1: message indicating what conditions is being waited for (e.g. 'config to be written')
+#  - 2: a string representing an eval'able condition.  When eval'd it should not output
+#       anything to stdout or stderr.
+#  - 3: optional timeout in seconds.  If not provided, waits forever.
+# Returns:
+#  1 if the condition is not met before the timeout
+function util::wait-for-condition() {
+  local msg=$1
+  # condition should be a string that can be eval'd.
+  local condition=$2
+  local timeout=${3:-}
+
+  local start_msg="Waiting for ${msg}"
+  local error_msg="[ERROR] Timeout waiting for ${msg}"
+
+  local counter=0
+  while ! ${condition}; do
+    if [[ "${counter}" = "0" ]]; then
+      echo -n "${start_msg}"
+    fi
+
+    if [[ -z "${timeout}" || "${counter}" -lt "${timeout}" ]]; then
+      counter=$((counter + 1))
+      if [[ -n "${timeout}" ]]; then
+        echo -n '.'
+      fi
+      sleep 1
+    else
+      echo -e "\n${error_msg}"
+      return 1
+    fi
+  done
+
+  if [[ "${counter}" != "0" && -n "${timeout}" ]]; then
+    echo ' done'
+  fi
+}
+readonly -f util::wait-for-condition
