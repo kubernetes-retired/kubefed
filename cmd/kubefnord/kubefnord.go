915c5f2f00f2bd1e286bbc1f5dc432071251cd6c
Ivan Font <ifont@redhat.com>
2018-04-23 17:12:43 -0700

    Enable auth for cloud providers

--- cmd/kubefnord/kubefnord.go
+++ cmd/kubefnord/kubefnord.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/kubernetes-sigs/federation-v2/pkg/kubefnord"
 	"k8s.io/apiserver/pkg/util/logs"
+	_ "k8s.io/client-go/plugin/pkg/client/auth" // Load all client auth plugins for GCP, Azure, etc
 )
 
 func main() {
