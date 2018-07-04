c7fad55f081cf09ed0d801891d6798de343b7a88
onyiny-ang <ltulloch@redhat.com>
2018-07-04 11:50:44 -0400

    Update image build to incorporate kubebuilder merge

--- scripts/imagebuild.sh
+++ scripts/imagebuild.sh
@@ -21,7 +21,6 @@ set -o pipefail
 base_dir="$(cd "$(dirname "$0")/.." ; pwd)"
 dockerfile_dir="${base_dir}/images/federation-v2"
 
-[ -f "$base_dir/bin/apiserver" ] || { echo "$base_dir/bin/apiserver not found" ; exit 1 ;}
 [ -f "$base_dir/bin/controller-manager" ] || { echo "$base_dir/bin/controller-manager not found" ; exit 1 ;}
 
 if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
@@ -32,8 +31,7 @@ fi
 echo "Starting image build"
 export REGISTRY=quay.io/
 export REPO=kubernetes-multicluster
-echo "Copy apiserver"
-cp ${base_dir}/bin/apiserver ${dockerfile_dir}/apiserver
+
 
 echo "Copy controller manager"
 cp ${base_dir}/bin/controller-manager ${dockerfile_dir}/controller-manager
@@ -45,5 +43,4 @@ echo "Pushing images with default tags (git sha and 'canary')."
 docker build ${dockerfile_dir} -t ${REGISTRY}${REPO}/federation-v2:canary
 docker push ${REGISTRY}${REPO}/federation-v2:canary
 
-rm ${dockerfile_dir}/apiserver
 rm ${dockerfile_dir}/controller-manager
