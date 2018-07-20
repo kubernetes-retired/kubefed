bb51aa73c75e033f7fed44a787c256c687416b29
shashidharatd <shashidhara.td@huawei.com>
2018-07-20 20:28:50 +0530

    Updated generated code

--- pkg/apis/multiclusterdns/v1alpha1/zz_generated.kubebuilder.go
+++ pkg/apis/multiclusterdns/v1alpha1/zz_generated.kubebuilder.go
@@ -105,8 +105,40 @@ var (
 							Type: "object",
 						},
 						"spec": v1beta1.JSONSchemaProps{
-							Type:       "object",
-							Properties: map[string]v1beta1.JSONSchemaProps{},
+							Type: "object",
+							Properties: map[string]v1beta1.JSONSchemaProps{
+								"endpoints": v1beta1.JSONSchemaProps{
+									Type: "array",
+									Items: &v1beta1.JSONSchemaPropsOrArray{
+										Schema: &v1beta1.JSONSchemaProps{
+											Type: "object",
+											Properties: map[string]v1beta1.JSONSchemaProps{
+												"dnsName": v1beta1.JSONSchemaProps{
+													Type: "string",
+												},
+												"labels": v1beta1.JSONSchemaProps{
+													Type: "object",
+												},
+												"recordTTL": v1beta1.JSONSchemaProps{
+													Type:   "integer",
+													Format: "int64",
+												},
+												"recordType": v1beta1.JSONSchemaProps{
+													Type: "string",
+												},
+												"targets": v1beta1.JSONSchemaProps{
+													Type: "array",
+													Items: &v1beta1.JSONSchemaPropsOrArray{
+														Schema: &v1beta1.JSONSchemaProps{
+															Type: "string",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
 						},
 						"status": v1beta1.JSONSchemaProps{
 							Type:       "object",
