85eef10b2a7b24b79288f08ff9393ede919989f8
Daneyon Hansen <danehans@cisco.com>
2018-09-04 18:00:20 +0000

    Removes unneeded kubebuilder CRD changes due to k8s issue #65293

--- pkg/apis/core/v1alpha1/zz_generated.kubebuilder.go
+++ pkg/apis/core/v1alpha1/zz_generated.kubebuilder.go
@@ -323,7 +323,6 @@ var (
 			Scope: "Namespaced",
 			Validation: &v1beta1.CustomResourceValidation{
 				OpenAPIV3Schema: &v1beta1.JSONSchemaProps{
-					Type: "object",
 					Properties: map[string]v1beta1.JSONSchemaProps{
 						"apiVersion": v1beta1.JSONSchemaProps{
 							Type: "string",
@@ -1621,7 +1620,6 @@ var (
 			Scope: "Namespaced",
 			Validation: &v1beta1.CustomResourceValidation{
 				OpenAPIV3Schema: &v1beta1.JSONSchemaProps{
-					Type: "object",
 					Properties: map[string]v1beta1.JSONSchemaProps{
 						"apiVersion": v1beta1.JSONSchemaProps{
 							Type: "string",
--- pkg/apis/multiclusterdns/v1alpha1/zz_generated.kubebuilder.go
+++ pkg/apis/multiclusterdns/v1alpha1/zz_generated.kubebuilder.go
@@ -174,7 +174,6 @@ var (
 			Scope: "Namespaced",
 			Validation: &v1beta1.CustomResourceValidation{
 				OpenAPIV3Schema: &v1beta1.JSONSchemaProps{
-					Type: "object",
 					Properties: map[string]v1beta1.JSONSchemaProps{
 						"apiVersion": v1beta1.JSONSchemaProps{
 							Type: "string",
@@ -247,7 +246,6 @@ var (
 			Scope: "Namespaced",
 			Validation: &v1beta1.CustomResourceValidation{
 				OpenAPIV3Schema: &v1beta1.JSONSchemaProps{
-					Type: "object",
 					Properties: map[string]v1beta1.JSONSchemaProps{
 						"apiVersion": v1beta1.JSONSchemaProps{
 							Type: "string",
