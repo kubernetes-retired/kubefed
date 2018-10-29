7be81ca7f2fe9859ca0af872b6137d3b37d926a2
mooncake <xcoder@tenxcloud.com>
2018-10-29 23:50:14 +0800

    Move the package comments above package
    Signed-off-by: mooncake <xcoder@tenxcloud.com>

--- hack/imports.go
+++ hack/imports.go
@@ -14,10 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package hack
-
 /*
-Package imports imports dependencies required for "dep ensure" to fetch all of the go package dependencies needed
+Package imports dependencies required for "dep ensure" to fetch all of the go package dependencies needed
 by kubebuilder commands to work without rerunning "dep ensure".
 
 Example: make sure the testing libraries and apimachinery libraries are fetched by "dep ensure" so that
@@ -26,4 +24,6 @@ dep ensure doesn't need to be rerun after "kubebuilder create resource".
 This is necessary for subsequent commands - such as building docs, tests, etc - to work without rerunning "dep ensure"
 afterward.
 */
+package hack
+
 import _ "github.com/kubernetes-sigs/kubebuilder/pkg/imports"
