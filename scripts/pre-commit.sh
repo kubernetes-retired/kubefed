3d4194971fff7b24a5257e2b44d9c134d9849b22
poothia <shubhendu.poothia@india.nec.com>
2019-01-29 16:20:20 +0530

    Adding CI Check for verification of use of correct Errors Package

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -127,6 +127,9 @@ check-git-state
 echo "Verifying Gofmt"
 ./hack/go-tools/verify-gofmt.sh
 
+echo "Checking that correct Error Package is used."
+./hack/verify-errpkg.sh
+
 echo "Checking that 'kubebuilder generate' is up-to-date"
 check-kubebuilder-output
 
