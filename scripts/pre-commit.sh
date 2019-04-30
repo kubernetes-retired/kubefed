340777e8df29cd581bd021dd8f662385cd48a92d
Maru Newby <marun@redhat.com>
2019-04-30 15:55:52 -0700

    Target Kubernetes 1.14 in CI

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -132,7 +132,7 @@ echo "Downloading e2e test dependencies"
 ./scripts/download-e2e-binaries.sh
 
 CREATE_INSECURE_REGISTRY=y CONFIGURE_INSECURE_REGISTRY=y OVERWRITE_KUBECONFIG=y \
-    ./scripts/create-clusters.sh
+    KIND_TAG="v1.14.0" ./scripts/create-clusters.sh
 
 # Initialize list of clusters to join
 join-cluster-list > /dev/null
