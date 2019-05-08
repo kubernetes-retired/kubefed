ed7bad75e313e1e80f8469db861b1a45396b097b
Ivan Font <ifont@redhat.com>
2019-05-08 12:23:06 -0700

    Convert webhook DaemonSet to Deployment

--- scripts/deploy-federation.sh
+++ scripts/deploy-federation.sh
@@ -112,8 +112,8 @@ function helm-deploy-cmd {
 }
 
 function kubefed-admission-webhook-ready() {
-  local numReady=$(kubectl -n ${1} get daemonsets.apps kubefed-admission-webhook -o jsonpath='{.status.numberReady}')
-  [[ "${numReady}" == "1" ]]
+  local readyReplicas=$(kubectl -n ${1} get deployments.apps kubefed-admission-webhook -o jsonpath='{.status.readyReplicas}')
+  [[ "${readyReplicas}" -ge "1" ]]
 }
 
 NS="${KUBEFED_NAMESPACE:-kube-federation-system}"
