c335b055144203cdb97a5cda03a3961626267955
poothia <shubhendu.poothia@india.nec.com>
2019-06-03 16:48:48 +0530

    Gate Check for Klog package

---
+++ hack/verify-klog.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(cd "${SCRIPT_ROOT}/../.." && pwd)"
+pushd ${REPO_ROOT} > /dev/null
+
+find_files() {
+  find . -not \( \
+      \( \
+        -wholename '*/vendor/*' \
+        -o -wholename '*/pkg/client/*' \
+        -o -wholename '*/doc.go' \
+      \) -prune \
+    \) -name '*.go'
+}
+
+ERRORPKG="grep -l glog"
+bad_files=$(find_files | xargs $ERRORPKG) || true
+popd > /dev/null
+
+if [[ -n "${bad_files}" ]]; then
+  echo "Please Switch from glog to klog to fix the following files:"
+  echo "${bad_files}"
+  exit 1
+fi
